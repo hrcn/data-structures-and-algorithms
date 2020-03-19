91844a907734b6282d2e7c595def4929f50f1fbd
Rui Han <hrcn1128@gmail.com>
2020-03-19 17:24:41 -0400

    stack & queue

--- Analysis of Algorithms/src/oneSum.java
+++
@@ -1,9 +0,0 @@
-public class oneSum {
-    public static int count(int[] a) {
-        int count = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == 0) count++;
-        }
-        return count;
-    }
-}
