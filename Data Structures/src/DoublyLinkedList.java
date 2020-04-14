eb5c9757e956b12ab8327adec0d83ab967267f9c
Rui Han <hrcn1128@gmail.com>
2020-04-14 11:27:53 -0400

    added doubly linked list

---
+++ Data Structures/src/DoublyLinkedList.java
@@ -0,0 +1,100 @@
+import java.util.NoSuchElementException;
+
+public class DoublyLinkedList {
+
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+
+    private class ListNode {
+        private int data;
+        private ListNode next;
+        private ListNode previous;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public void displayForward() {
+        if (head == null) return;
+
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.println(temp.data + " --> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void displayBackward() {
+        if (tail == null) return;
+
+        ListNode temp = tail;
+        while (temp != null) {
+            System.out.println(temp.data + " --> ");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+    }
+
+    public void insertFirst(int value) {
+        ListNode newNode = new ListNode(value);
+
+        if (isEmpty()) {
+            tail = newNode;
+        } else {
+            head.previous = newNode;
+        }
+
+        newNode.previous = head;
+        head = newNode;
+        length++;
+    }
+
+    public void insertLast(int value) {
+        ListNode newNode = new ListNode(value);
+
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+    public ListNode deleteFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        ListNode temp = head;
+        // Doubly linked list has only one node
+        if (head == tail) {
+            tail = null;
+        } else {
+            head.next.previous = null;
+        }
+        head = head.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+}
