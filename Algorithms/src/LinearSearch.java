1818c5ec142f14970ddab8ffd58cc50b72dde8cb
Rui Han <hrcn1128@gmail.com>
2020-04-24 21:28:16 -0400

    linear search

---
+++ Algorithms/src/LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch {
+    public int search(int[] arr, int x) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 9, 2, 10, 15, 20};
+        LinearSearch ls = new LinearSearch();
+        System.out.println(ls.search(arr, 15));
+        System.out.println(ls.search(arr, 50));
+    }
+}
