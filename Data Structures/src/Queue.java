ead40a3d85fef9e6a4846b0c54d596a42def8631
Rui Han <hrcn1128@gmail.com>
2020-05-07 18:43:52 -0400

    updated queue

--- Data Structures/src/Queue.java
+++ Data Structures/src/Queue.java
@@ -1,40 +1,88 @@
+import java.util.NoSuchElementException;
+
 public class Queue {
-    private static class Node {
+
+    private ListNode front;
+    private ListNode rear;
+    private int length;
+
+    private class ListNode {
         private int data;
-        private Node next;
-        private Node(int data) {
+        private ListNode next;
+
+        public ListNode(int data) {
             this.data = data;
+            this.next = null;
         }
     }
 
-    private Node head;
-    private Node tail;
+    public int length() {
+        return length;
+    }
 
     public boolean isEmpty() {
-        return head == null;
+        return length == 0;
     }
 
-    public int peek() {
-        return head.data;
+    public void print() {
+        if (isEmpty()) return;
+
+        ListNode current = front;
+        while (current != null) {
+            System.out.println(current.data + " --> ");
+            current = current.next;
+        }
+        System.out.println("null");
     }
 
-    public void add(int data) {
-        Node node = new Node(data);
-        if(tail != null) {
-            tail.next = node;
+    public void enqueue(int data) {
+        ListNode temp = new ListNode(data);
+        if (isEmpty()) {
+            front = temp;
+        } else {
+            rear.next = temp;
         }
-        tail = node;
-        if(head == null) {
-            head = node;
-        }
+        rear = temp;
+        length++;
     }
 
-    public int remove() {
-        int data = head.data;
-        head = head.next;
-        if(head == null) {
-            tail = null;
-        }
-        return data;
+    public int dequeue() {
+        if (isEmpty()) throw new NoSuchElementException();
+
+        int result = front.data;
+        front = front.next;
+        if (front == null) rear = null;
+        length--;
+        return result;
+    }
+
+    public int first() {
+        if (isEmpty()) throw new NoSuchElementException("Queue is already empty");
+
+        return front.data;
+    }
+
+    public int last() {
+        if (isEmpty()) throw new NoSuchElementException("Queue is already empty");
+
+        return rear.data;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue(10);
+        queue.enqueue(15);
+        queue.enqueue(20);
+        queue.enqueue(25);
+
+        queue.print();
+
+        queue.dequeue();
+        queue.dequeue();
+
+        queue.print();
+
+        System.out.println(queue.first());
+        System.out.println(queue.last());
     }
 }
